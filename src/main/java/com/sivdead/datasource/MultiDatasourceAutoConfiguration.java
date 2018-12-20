1cbd6bb97b705f480c5d15023b0a6de376aa156b
luo.qianhong <jinwen.lqh@dtyunxi.com>
2018-12-21 06:57:52 +0800

    支持多种数据源类型

--- src/main/java/com/sivdead/datasource/MultiDatasourceAutoConfiguration.java
+++ src/main/java/com/sivdead/datasource/MultiDatasourceAutoConfiguration.java
@@ -1,13 +1,14 @@
 package com.sivdead.datasource;
 
-import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ClassUtils;
 
 import javax.sql.DataSource;
 import java.util.Hashtable;
@@ -19,7 +20,7 @@ import java.util.Map;
  */
 @Configuration
 @EnableConfigurationProperties(MultiDatasourceProperties.class)
-@ConditionalOnClass({ DataSource.class, HikariDataSource.class})
+@ConditionalOnClass({DataSource.class})
 public class MultiDatasourceAutoConfiguration {
 
 
@@ -44,6 +45,10 @@ public class MultiDatasourceAutoConfiguration {
         if (properties == null){
             return null;
         }
-        return properties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+        Class<? extends DataSource> datasourceType = properties.getType();
+        if (datasourceType == null) {
+            datasourceType = DataSourceBuilder.findType(ClassUtils.getDefaultClassLoader());
+        }
+        return properties.initializeDataSourceBuilder().type(datasourceType).build();
     }
 }
