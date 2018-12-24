d845e32b94bd50cc88c54fa18b48d58c3e7818ee
luo.qianhong <jinwen.lqh@dtyunxi.com>
2018-12-24 17:23:58 +0800

    多数据源健康检查 优化,修复引入mybatis page helper报错的问题

--- src/main/java/com/sivdead/datasource/MultiDatasourceAutoConfiguration.java
+++ src/main/java/com/sivdead/datasource/MultiDatasourceAutoConfiguration.java
@@ -1,19 +1,27 @@
 package com.sivdead.datasource;
 
-import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.health.ConditionalOnEnabledHealthIndicator;
+import org.springframework.boot.actuate.health.*;
+import org.springframework.boot.actuate.jdbc.DataSourceHealthIndicator;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.jdbc.metadata.CompositeDataSourcePoolMetadataProvider;
+import org.springframework.boot.jdbc.metadata.DataSourcePoolMetadata;
 import org.springframework.boot.jdbc.metadata.DataSourcePoolMetadataProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.ClassUtils;
 
 import javax.sql.DataSource;
+import java.util.Collection;
 import java.util.Hashtable;
 import java.util.Map;
 
@@ -21,20 +29,34 @@ import java.util.Map;
  * @author 敬文
  * @date 2018/12/20
  */
-@Configuration
+@Configuration("dataSourceHealthIndicatorAutoConfiguration")
 @EnableConfigurationProperties(MultiDatasourceProperties.class)
 @ConditionalOnClass({DataSource.class})
-public class MultiDatasourceAutoConfiguration {
+@ConditionalOnProperty(prefix = "multi-datasource", name = "enable")
+@AutoConfigureBefore({DataSourceAutoConfiguration.class})
+public class MultiDatasourceAutoConfiguration implements InitializingBean {
+
+    private final Collection<DataSourcePoolMetadataProvider> metadataProviders;
+
+    private final HealthAggregator healthAggregator;
+
+    private DataSourcePoolMetadataProvider poolMetadataProvider;
+
+    @Autowired
+    public MultiDatasourceAutoConfiguration(Collection<DataSourcePoolMetadataProvider> metadataProviders, HealthAggregator healthAggregator) {
+        this.metadataProviders = metadataProviders;
+        this.healthAggregator = healthAggregator;
+    }
 
 
     @ConditionalOnMissingBean({DataSource.class})
-    @ConditionalOnProperty(prefix = "multi-datasource", name = "enable")
     @Bean
-    public DataSource multiDatasource(MultiDatasourceProperties properties) {
+    public DynamicDataSource multiDatasource(MultiDatasourceProperties properties) {
         DynamicDataSource dynamicDataSource = new DynamicDataSource();
         Map<Object, Object> dataSourceMap = new Hashtable<>();
         for (Map.Entry<String, DataSourceProperties> entry : properties.getDatasourceMap().entrySet()) {
-            dataSourceMap.put(entry.getKey(), createDatasource(entry.getValue()));
+            DataSource datasource = createDatasource(entry.getValue());
+            dataSourceMap.put(entry.getKey(), datasource);
         }
         dynamicDataSource.setTargetDataSources(dataSourceMap);
         if (properties.getDefaultDatasource() != null) {
@@ -45,18 +67,16 @@ public class MultiDatasourceAutoConfiguration {
     }
 
     @Bean
-    public MultiDataSourceHealthIndicatorConfiguration dataSourceHealthIndicatorConfiguration(
-            ObjectProvider<Map<String, DataSource>> dataSources,
-            ObjectProvider<DataSourcePoolMetadataProvider> metadataProviders) {
-        return new MultiDataSourceHealthIndicatorConfiguration(dataSources, metadataProviders);
-    }
-
-    @Bean
+    @ConditionalOnEnabledHealthIndicator("db")
     @ConditionalOnMissingBean(
             name = {"dbHealthIndicator"}
     )
-    public HealthIndicator dbHealthIndicator(MultiDataSourceHealthIndicatorConfiguration dataSourceHealthIndicatorConfiguration) {
-        return dataSourceHealthIndicatorConfiguration.dbHealthIndicator();
+    public HealthIndicator dbHealthIndicator(DynamicDataSource dataSource) {
+        HealthIndicatorRegistry registry = new DefaultHealthIndicatorRegistry();
+        dataSource.getTargetDataSources().forEach(
+                (name, source) ->
+                        registry.register((String) name, new DataSourceHealthIndicator((DataSource) source, getValidationQuery((DataSource) source))));
+        return new CompositeHealthIndicator(this.healthAggregator, registry);
     }
 
     private DataSource createDatasource(DataSourceProperties properties) {
@@ -68,5 +88,18 @@ public class MultiDatasourceAutoConfiguration {
             datasourceType = DataSourceBuilder.findType(ClassUtils.getDefaultClassLoader());
         }
         return properties.initializeDataSourceBuilder().type(datasourceType).build();
+    }
+
+
+    @Override
+    public void afterPropertiesSet() {
+        this.poolMetadataProvider = new CompositeDataSourcePoolMetadataProvider(
+                this.metadataProviders);
+    }
+
+    private String getValidationQuery(DataSource source) {
+        DataSourcePoolMetadata poolMetadata = this.poolMetadataProvider
+                .getDataSourcePoolMetadata(source);
+        return (poolMetadata != null) ? poolMetadata.getValidationQuery() : null;
     }
 }
